e4ad7701656712756778a23f0a07de364726985f
Vishal Patel <vpatel314@gmail.com>
2018-12-28 10:02:39 -0800

    Updated test

--- test_asapycovstorage.py
+++ test_asapycovstorage.py
@@ -1,15 +1,21 @@
 from unittest import TestCase
+import unittest
 from ASAPy import AsapyCovStorage
 
 class TestCreate_stddev_df(TestCase):
     def test_construct(self):
         df = AsapyCovStorage.create_stddev_df(5)
-        self.assertListEqual(list(df.columns), ['groups', 'e high', 'x-sec(1)', 'rel.s.d.(1)', 's.d.(1)'])
-        self.assertListEqual(list(df.index), list(range(5)))
+        self.assertListEqual(list(df.columns),
+                             ['e low', 'e high', 'x-sec(1)', 'x-sec(2)', 'rel.s.d.(1)', 'rel.s.d(2)', 's.d.(1)',
+                              's.d(2)'])
+        self.assertListEqual(list(df.index), list(range(1, 6)))
 
 
 class TestCreate_corr_df(TestCase):
     def test_construct(self):
         df = AsapyCovStorage.create_corr_df(5)
-        self.assertListEqual(list(df.columns), list(range(5)))
-        self.assertListEqual(list(df.index), list(range(5)))
+        self.assertListEqual(list(df.columns), list(range(1, 6)))
+        self.assertListEqual(list(df.index), list(range(1, 6)))
+
+if __name__ == "__main__":
+    unittest.main()
